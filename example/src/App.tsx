fe3c48a6744c2f65305dc296b56e9cec20b418e7
Odenir <odenirdev@gmail.com>
2020-10-22 17:23:18 -0300

    v1.0.0

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import Styled from 'styled-components'
+import './styles.css'
 
-import { ExampleComponent } from 'react-og-buttons'
-import 'react-og-buttons/dist/index.css'
+import Button from 'react-og-buttons'
+
+import WebFont from 'webfontloader'
+
+WebFont.load({
+  google: {
+    families: ['Roboto:300,400,700']
+  }
+})
+
+const Grid = Styled.main`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    button {
+        margin-top: 24px;
+    }
+`
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <Grid>
+      <Button>OG Button</Button>
+      <Button backgroundColor='#2196f3' color='#fafafa'>
+        Edit Button
+      </Button>
+      <Button typeStyle='success'>Success Button</Button>
+      <Button typeStyle='outline'>Outline Button</Button>
+      <Button typeStyle='warning'>Warning Button</Button>
+      <Button typeStyle='danger'>Danger Button</Button>
+    </Grid>
+  )
 }
 
 export default App
